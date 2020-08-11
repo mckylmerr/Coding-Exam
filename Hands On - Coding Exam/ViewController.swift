3040c00319ac49c9bf2b8a998729c5c6bdcdf08a
Moesha King <moeshachance@gmail.com>
2020-08-11 16:37:55 +0800

    Initial Commit

---
+++ Hands On - Coding Exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hands On - Coding Exam
+//
+//  Created by Mc kylmerr Ico on 11/08/2020.
+//  Copyright © 2020 Mc kylmerr Ico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
